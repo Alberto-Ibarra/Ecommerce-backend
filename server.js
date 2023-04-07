6de147fef4ccee46238b0a4f3aef9d56995b3980
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2023-04-07 09:14:27 -0700

    added jwt token to user logging in

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes')
 
 
 dotenv.config()
 connectDB()
 
-
 const app = express();
+app.use(express.json())
 
 const port = process.env.PORT || 5000;
 
@@ -21,6 +22,8 @@ app.get('/', (req,res)=>{
 
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+
 
 
 app.listen(port, console.log(`server running in ${process.env.NODE_ENV} on port ${port}`))

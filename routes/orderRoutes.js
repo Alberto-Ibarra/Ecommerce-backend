5291200b53e1edae15276abef0b97d01ef029a67
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2023-04-11 15:34:19 -0700

    update to paid endpoint

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {protect} = require('../middleware/authMiddleware')
-const {addOrderItems, getOrderById} = require('../controllers/orderController')
+const {addOrderItems, getOrderById, updateOrderToPaid} = require('../controllers/orderController')
 
 //POST login
 router.post('/', protect, addOrderItems)
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
 
 module.exports = router
